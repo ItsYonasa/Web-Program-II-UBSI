4ec800891c10622eaabbfa56a05eea12ce956894
ItsYonasa <90910875+ItsYonasa@users.noreply.github.com>
2022-06-23 19:30:14 +0700

    Tugas 1

---
+++ Tugas 1/CodeIgniter-3.1.11/application/controllers/contoh1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ */
+class Contoh1 extends CI_Controller
+{
+
+  public function index()
+    {
+      echo "<h1>Perkenalkan</h1>";
+      echo "Nama Saya Yoseph Prajna Sanjaya";
+      echo "Kelas: 17.6A.25";
+      echo "NIM: 17190051";
+      echo "Tinggal: Jln. Utama 1 No. 32
+            hobi Bermain Dota 2";  
+    }
